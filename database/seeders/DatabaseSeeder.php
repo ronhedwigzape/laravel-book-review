97b935bd72b2e3e5a3ba746a4201f7d136d40e3e
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-21 10:37:59 +0800

    feat: add factories and seeders
    Artisan command:
    
    Used for fake data/s:
    
    - `php artisan make:factory BookFactory --model=Book`
    
    - `php artisan make:factory ReviewFactory --model=Review`

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,34 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Book::factory(33)->create()->each(function ($book) {
+           $numReviews = random_int(5, 30);
+
+           Review::factory()
+               ->count($numReviews)
+               ->good()
+               ->for($book)
+               ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()
+                ->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()
+                ->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
     }
 }

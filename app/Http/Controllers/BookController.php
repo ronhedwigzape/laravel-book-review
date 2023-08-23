321bdc59b3267193da86356367cf9852a8bafce8
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-23 10:33:30 +0800

    feat: show the cached data

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -28,7 +28,12 @@ class BookController extends Controller
             default => $books->latest()
         };
 
-        $books = $books->get();
+//        $books = $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, function () use($books) {
+            dd('Not from cache');
+            return $books->get();
+        });
 
         return view('books.index', ['books' => $books]);
     }

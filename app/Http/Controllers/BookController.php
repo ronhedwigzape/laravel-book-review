bb9898d68081dcd80630fbb2d92e7884a95fd0d7
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-23 10:34:44 +0800

    refactor: optimize code for caching

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -30,10 +30,7 @@ class BookController extends Controller
 
 //        $books = $books->get();
         $cacheKey = 'books:' . $filter . ':' . $title;
-        $books = cache()->remember($cacheKey, 3600, function () use($books) {
-            dd('Not from cache');
-            return $books->get();
-        });
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
